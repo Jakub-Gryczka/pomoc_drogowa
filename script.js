6c628097d5a9bbc6edf26c641b5288765cf7bff4
Jakub <gryczka.jakub@gmail.com>
2023-08-20 01:06:37 +0200

    finished advantages section, have to add script to it, started doing services section

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ window.onload = function () {
     nav.classList.toggle("is-active");
   });
 };
+
+// WORKING ON SCROLL, HAVE TO ADD ANIMATION
+// const element = window;
+// const output = document.querySelector('p.output');
+
+// element.addEventListener("scroll", (event) => {
+//   output.innerHTML = "scroll event fired!";
+//   setTimeout(() => {
+//     output.innerHTML = "Waiting on scroll events...";
+//   }, 1000);
+// });
