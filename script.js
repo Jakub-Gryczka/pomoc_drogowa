e313d64cc3170b0639c2683b767eb676f395ed68
Jakub <gryczka.jakub@gmail.com>
2023-08-22 17:08:46 +0200

    added intersection observer to script

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ window.onload = function () {
   const nav = document.querySelector(".nav");
   const navItem = document.querySelectorAll(".nav__link");
   const navBtn = document.querySelector(".nav__btn");
+  const number = document.querySelectorAll(".number");
+  const advantages = document.querySelector(".advantages");
 
   toggleAndRemoveIsActive = () => {
     menu_btn.classList.toggle("is-active");
     nav.classList.remove("is-active");
-  }
+  };
 
-
-  for(let i = 0; i < navItem.length; i++) {
+  for (let i = 0; i < navItem.length; i++) {
     navItem[i].addEventListener("click", function () {
-     toggleAndRemoveIsActive();
+      toggleAndRemoveIsActive();
     });
   }
 
@@ -25,73 +26,33 @@ window.onload = function () {
     nav.classList.toggle("is-active");
   });
 
-  const number = document.querySelectorAll(".advantages__item__number");
-  const speed = 5000;
+  const speed = 175;
   function counter() {
-    number.forEach(element => {
+    number.forEach((element) => {
       incNumber(element);
     });
     function incNumber(elem) {
       let text = +elem.innerText;
-      const value = +elem.getAttribute("data-target");
-      const inc = Math.ceil(value / speed / 2);
-      if(text < value) {
+      const value = +elem.getAttribute("data-count");
+      const inc = Math.ceil(value / speed);
+      if (text < value) {
         elem.innerText = inc + text;
         setTimeout(() => {
           incNumber(elem);
         }, 20);
       } else {
-        elem.style.marginLeft = "0.5em";
-        elem.innerText = value + "+";
+        elem.innerText = value;
       }
     }
-}
+  }
 
-// counter();
-
-
-  function isInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-
-    );
-}
-
-
-const box = document.querySelector('.advantages');
-const message = document.querySelector('#message');
-
-document.addEventListener('scroll', function () {
-    if(isInViewport(box)) {
+  function obCallback(payload) {
+    if (payload[0].intersectionRatio > 0.5035) {
       counter();
     }
-
-}, {
-    passive: true
-});
-
-
-
+  }
+  const ob = new IntersectionObserver(obCallback, {
+    threshold: [0, 0.5, 1],
+  });
+  ob.observe(advantages);
 };
-
-
-
-
-
-
-
-
-// WORKING ON SCROLL, HAVE TO ADD ANIMATION
-// const element = window;
-// const output = document.querySelector('p.output');
-
-// element.addEventListener("scroll", (event) => {
-//   output.innerHTML = "scroll event fired!";
-//   setTimeout(() => {
-//     output.innerHTML = "Waiting on scroll events...";
-//   }, 1000);
-// });
