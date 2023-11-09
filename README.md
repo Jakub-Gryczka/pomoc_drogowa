bcd5929e429b57af8fc44d99156708cb76e2279b
Jakub <gryczka.jakub@gmail.com>
2023-11-09 19:33:02 +0100

    final page

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-#Roadside Assistance Website (pomoc-drogowa.lukow.pl) - commercial project
+# Roadside Assistance Website (pomoc-drogowa.lukow.pl) - commercial project
+
 It's my first commercial website made for real company. The client wanted to refresh his actual website and I volunteered to make it. The aim of the project is to attract clients for roadside company. I made a design for the website in Figma as well as provided domain, hosting, sitemap and used Google Search Console to test the app in terms of SEO, responsiveness etc.
-##Technologies
+
+## Technologies
 
 - HTML
 - CSS
 - JAVASCRIPT
+- SEO
+
   I was planning on using Sass or Tailwind, but I decided not to use technologies I know well in commercial project.
-  ##Live website
+
+  ## Live website
+
   https://pomoc-drogowa.lukow.pl/
---
+++ src/js/script.js
@@ -0,0 +1,84 @@
+"use strict";
+
+window.onload = function () {
+  const logo = document.querySelector(".logo");
+  const menu_btn = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav");
+  const navItem = document.querySelectorAll(".nav__link");
+  const navBtn = document.querySelector(".nav__btn");
+  const number = document.querySelectorAll(".number");
+  const advantages = document.querySelector(".advantages");
+
+  const toggleAndRemoveIsActive = () => {
+    menu_btn.classList.toggle("is-active");
+    nav.classList.remove("is-active");
+  };
+
+  for (let i = 0; i < navItem.length; i++) {
+    navItem[i].addEventListener("click", function () {
+      toggleAndRemoveIsActive();
+    });
+  }
+
+  navBtn.addEventListener("click", function () {
+    toggleAndRemoveIsActive();
+  });
+
+  menu_btn.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    nav.classList.toggle("is-active");
+  });
+
+  document.addEventListener("scroll", function () {
+    nav.classList.remove("is-active");
+    menu_btn.classList.remove("is-active");
+  });
+
+  const speed = 175;
+  function counter() {
+    number.forEach((element) => {
+      incNumber(element);
+    });
+    function incNumber(elem) {
+      let text = +elem.innerText;
+      const value = +elem.getAttribute("data-count");
+      const inc = Math.ceil(value / speed);
+      if (text < value) {
+        elem.innerText = inc + text;
+        setTimeout(() => {
+          incNumber(elem);
+        }, 20);
+      } else {
+        elem.innerText = value;
+      }
+    }
+  }
+
+  function countOnScroll(payload) {
+    if (payload[0].intersectionRatio > 0.5035) {
+      counter();
+    }
+  }
+
+  function hideNav(payload) {
+    if (payload[0].intersectionRatio === 1) {
+      document.addEventListener("click", function handleClickOutsideBox(event) {
+        if (!nav.contains(event.target) && !logo.contains(event.target)) {
+          nav.classList.remove("is-active");
+          menu_btn.classList.remove("is-active");
+        }
+      });
+    }
+  }
+
+  const ob = new IntersectionObserver(countOnScroll, {
+    threshold: [0, 0.5, 1],
+  });
+
+  const navOb = new IntersectionObserver(hideNav, {
+    threshold: [0, 0.5, 1],
+  });
+
+  ob.observe(advantages);
+  navOb.observe(nav);
+};
